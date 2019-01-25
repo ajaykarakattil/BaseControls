3acf99e238812bf78dae90340c0b6ea59f181e5a
ajaykarakattil <ajaymohan24@gmail.com>
2019-01-25 23:33:35 +0530

    25/01/2019

--- app/src/main/java/com/example/darkroom/basecontrols/DispMsgActivity.java
+++ app/src/main/java/com/example/darkroom/basecontrols/DispMsgActivity.java
@@ -3,10 +3,11 @@ package com.example.darkroom.basecontrols;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class DispMsgActivity extends AppCompatActivity {
-
+public static String MY_KEY="Key is a key";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +16,16 @@ public class DispMsgActivity extends AppCompatActivity {
         String msg=intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView tv=(TextView)findViewById(R.id.textView2);
         tv.setText(msg);
+
+    }
+    public void ac1(View v){
+        Intent resIntent=new Intent(this,Result_next.class);
+        TextView t =(TextView)findViewById(R.id.textView2);
+        String nextMsg= t.getText().toString();
+        resIntent.putExtra(MY_KEY,nextMsg);
+        startActivity(resIntent);
+
+
 
     }
 }
